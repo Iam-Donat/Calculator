27b1329c5811576ac1afb9a0124283b7eb66e01c
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-11-28 20:41:57 +0200

    some changes

--- script.js
+++ script.js
@@ -1,111 +1,138 @@
-// Getting HTML ELements
-const btns = document.querySelectorAll('.calculator button') // Get all buttons
+// // Getting HTML ELements
+// const btns = document.querySelectorAll('.calculator button') // Get all buttons
 
-// Display
-const displayTop = document.querySelector('.top');
-const displayBottom = document.querySelector('.bottom');
+// // Display
+// const displayTop = document.querySelector('.top');
+// const displayBottom = document.querySelector('.bottom');
 
-//Numbers and Operators
-const numBtnValues = document.querySelectorAll('.value');
-const operatorBtn = document.querySelectorAll('.operator');
+// //Numbers and Operators
+// const numBtnValues = document.querySelectorAll('.value');
+// const operatorBtn = document.querySelectorAll('.operator');
 
-// Action Keys
-const decimalOp = document.querySelector('.decimal');
-const equalBtn = document.querySelector('.equal');
-const clearBtn = document.querySelector('.clear')
-const deleteBtn = document.querySelector('.delete');
-const percentage = document.querySelector('.percentage');
+// // Action Keys
+// const decimalOp = document.querySelector('.decimal');
+// const equalBtn = document.querySelector('.equal');
+// const clearBtn = document.querySelector('.clear')
+// const deleteBtn = document.querySelector('.delete');
+// const percentage = document.querySelector('.percentage');
 
-// Initialize default Value
-let firstOperand = '';
-let secondOperand = '';
-let operation = '';
+// // Initialize default Value
+// let firstOperand = '';
+// let secondOperand = '';
+// let operation = '';
 
-// Functions
-function numValuesHandler(num) { // numbers value handler
-  // console.log(num);
-  // to control the length of the number
-  const MAX_LENGTH = 10;
-  if (firstOperand.length < MAX_LENGTH) {
-    firstOperand += num;
-  }
+// // Functions
+// function numValuesHandler(num) { // numbers value handler
+//   // console.log(num);
+//   // to control the length of the number
+//   const MAX_LENGTH = 10;
+//   if (firstOperand.length < MAX_LENGTH) {
+//     firstOperand += num;
+//   }
+// }
+
+// function operatorsHandler(operator) {
+//   // console.log(operator);
+//   operation = operator;
+//   secondOperand = firstOperand;
+//   firstOperand = '';
+// }
+
+// function operationHandler() {
+
+//   displayTop.textContent = `${secondOperand} ${operation}  ${firstOperand}`;
+
+//   secondOperand = Number(secondOperand);
+//   firstOperand = Number(firstOperand);
+
+//   switch (operation) {
+//     case '+':
+//       secondOperand += firstOperand;
+//       break;
+//     case '-':
+//       secondOperand -= firstOperand;
+//       break;
+//     case '*':
+//       secondOperand *= firstOperand;
+//       break;
+//     case '/':
+//       if (firstOperand === 0) {
+//         displayBottom.textContent = 'Error';
+//         return;
+//       }
+//       secondOperand /= firstOperand;
+//       break;
+//   }
+
+//   displayBottom.textContent = secondOperand;
+
+// }
+
+
+// // Events handlers
+// numBtnValues.forEach(num => {
+//   num.addEventListener('click', (e) => {
+//     numValuesHandler(e.target.textContent);
+
+//     displayBottom.textContent = firstOperand
+
+
+//   })
+// });
+
+// operatorBtn.forEach(operator => {
+//   operator.addEventListener('click', (e) => {
+//     operatorsHandler(e.target.textContent);
+
+//     displayTop.textContent = `${secondOperand}  ${operation}`;
+//     displayBottom.textContent = firstOperand;
+
+//   })
+// })
+
+// clearBtn.addEventListener('click', () => {
+//   firstOperand = '';
+//   secondOperand = '';
+//   operation = '';
+//   displayTop.textContent = firstOperand;
+//   displayBottom.textContent = firstOperand;
+// });
+
+// equalBtn.addEventListener('click', () => {
+//   operationHandler();
+// });
+
+// deleteBtn.addEventListener('click', handleDelete);
+
+// function handleDelete() {
+//   firstOperand = firstOperand.slice(0, -1);
+//   displayBottom.textContent = firstOperand;
+// }
+
+
+function countdownToChristmas() {
+  // Get today's date
+  const today = new Date();
+
+  // Set the target date as December 25th of the current year
+  const christmas = new Date(today.getFullYear(), 11, 25);
+
+  // Calculate the time difference between today and Christmas
+  const timeToChristmas = christmas.getTime() - today.getTime();
+
+  // Convert the time difference to days, hours, minutes, and seconds
+  const daysLeft = Math.floor(timeToChristmas / (1000 * 60 * 60 * 24));
+  const hoursLeft = Math.floor((timeToChristmas % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutesLeft = Math.floor((timeToChristmas % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsLeft = Math.floor((timeToChristmas % (1000 * 60)) / 1000);
+
+  // Format the countdown message
+  const countdownMessage = `${daysLeft} days, ${hoursLeft} hours, ${minutesLeft} minutes, and ${secondsLeft} seconds until Christmas!`;
+
+  // Return the countdown message
+  return countdownMessage;
 }
 
-function operatorsHandler(operator) {
-  // console.log(operator);
-  operation = operator;
-  secondOperand = firstOperand;
-  firstOperand = '';
-}
-
-function operationHandler() {
-
-  displayTop.textContent = `${secondOperand} ${operation}  ${firstOperand}`;
-
-  secondOperand = Number(secondOperand);
-  firstOperand = Number(firstOperand);
-
-  switch (operation) {
-    case '+':
-      secondOperand += firstOperand;
-      break;
-    case '-':
-      secondOperand -= firstOperand;
-      break;
-    case '*':
-      secondOperand *= firstOperand;
-      break;
-    case '/':
-      if (firstOperand === 0) {
-        displayBottom.textContent = 'Error';
-        return;
-      }
-      secondOperand /= firstOperand;
-      break;
-  }
-
-  displayBottom.textContent = secondOperand;
-
-}
-
-
-// Events handlers 
-numBtnValues.forEach(num => {
-  num.addEventListener('click', (e) => {
-    numValuesHandler(e.target.textContent);
-
-    displayBottom.textContent = firstOperand
-
-
-  })
-});
-
-operatorBtn.forEach(operator => {
-  operator.addEventListener('click', (e) => {
-    operatorsHandler(e.target.textContent);
-
-    displayTop.textContent = `${secondOperand}  ${operation}`;
-    displayBottom.textContent = firstOperand;
-
-  })
-})
-
-clearBtn.addEventListener('click', () => {
-  firstOperand = '';
-  secondOperand = '';
-  operation = '';
-  displayTop.textContent = firstOperand;
-  displayBottom.textContent = firstOperand;
-});
-
-equalBtn.addEventListener('click', () => {
-  operationHandler();
-});
-
-deleteBtn.addEventListener('click', handleDelete);
-
-function handleDelete() {
-  firstOperand = firstOperand.slice(0, -1);
-  displayBottom.textContent = firstOperand;
-}
+const countdownMessage = countdownToChristmas();
+console.log(countdownMessage);
 
